4dc4cb150c1e8d2f93eb15641e552361e6da9cab
Stanko <stanko@bezak.net>
2020-10-25 16:59:05 +0100

    nic

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -61,12 +61,6 @@ public class EnemyMovement : MonoBehaviour
             _pathDestValid = false;
             //Debug.Log("Move to player");
         }
-
-        if (state == EnemyState.Attack)
-        {
-            _path.Clear();
-            _pathDestValid = false;
-        }
         if (state == EnemyState.Patrol)
         {
             if (_path.Count == 0)
@@ -183,7 +177,6 @@ public class EnemyMovement : MonoBehaviour
         if (Vector2.Distance(_rb.position, Player.position) < attackRange)
         {
             state = EnemyState.Attack;
-            _pathDestValid = false;
             _rb.velocity = Vector2.zero;
         }
         else
@@ -212,10 +205,12 @@ public class EnemyMovement : MonoBehaviour
                 throw new ArgumentOutOfRangeException();
         }
         Gizmos.DrawWireSphere(transform.position, .5f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, _destination);
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, _pathDestination);
         var p = _path.ToArray();
-        for (int i = 0; i < _path.Count; i++)
+        for (int i = 0; i < _path.Count-1; i++)
         {
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(p[i], p[i+1]);
