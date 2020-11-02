05303226356d7dbc2efc8312eb90ff6c7cbedede
Stanko <stanko@bezak.net>
2020-11-02 14:24:14 +0100

    importnute filipove kresby

---
+++ Assets/Scripts/Missions/MissionUI.cs
@@ -0,0 +1,31 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Missions
+{
+    public class MissionUI : MonoBehaviour
+    {
+        public Mission mission;
+        public TextMeshProUGUI headerText;
+        public TextMeshProUGUI descriptionText;
+
+        private void Start()
+        {
+            headerText.SetText($"{mission.missionName}: {mission.locationName}");
+            descriptionText.SetText($"{mission.description} {GetMissionProgress()}");
+        }
+
+        private void Update()
+        {
+            descriptionText.SetText($"{mission.description} {GetMissionProgress()}");
+            if(mission.Complete) Destroy(gameObject);
+        }
+
+        private string GetMissionProgress()
+        {
+            if (mission.completion == 1) return "";
+            return $"({mission.GetProgress()}/{mission.completion})";
+        }
+    }
+}
